03b4e83268e98847eace6b7769c7fec7f54ce958
Ian White <ianwhitester@gmail.com>
2018-08-18 17:59:19 -0700

    Send pipeline errors to boombox-pipeline-errors@googlegroups.com

--- pipeline/functions/aws.py
+++ pipeline/functions/aws.py
@@ -11,6 +11,7 @@ from pynamodb.exceptions import DoesNotExist
 from functions.utils.pynamodb_models import EpisodeModel
 from functions.utils.log_cfg import logger
 from functions.utils.constants import WORD, SPEAKER, CONFIDENCE, START_TIME, END_TIME
+from functions.utils.utils import logError
 
 s3 = boto3.resource('s3')
 sns = boto3.resource('sns')
@@ -55,127 +56,131 @@ def dateconverter(o):
 
 def transcribe(event, context):
     logger.debug(json.dumps(event))
+    try:
+        REGION = os.environ['REGION']
+        INPUT_BUCKET = os.environ['INPUT_BUCKET']
+        OUTPUT_BUCKET = os.environ['OUTPUT_BUCKET']
 
-    logger.debug('boto3 version: ' + boto3.__version__)
-    logger.debug('botocore version: ' + botocore.__version__)
-
-    REGION = os.environ['REGION']
-    INPUT_BUCKET = os.environ['INPUT_BUCKET']
-    OUTPUT_BUCKET = os.environ['OUTPUT_BUCKET']
-
-    for record in event['Records']:
-        message = json.loads(record['Sns']['Message'])
-        for output in message['outputs']:
-            key = output['key']
-            uri = 'https://s3-' + REGION + '.amazonaws.com/' + INPUT_BUCKET + '/' + key
-            jobName = key.split('.')[0]
-            response = transcribeService.start_transcription_job(
-                TranscriptionJobName=str(
-                    uuid.uuid4()) + '-' + getSafeGUID(jobName),
-                LanguageCode='en-US',
-                MediaSampleRateHertz=44100,
-                MediaFormat='mp3',
-                Media={
-                    'MediaFileUri': uri
-                },
-                OutputBucketName=OUTPUT_BUCKET,
-                Settings={
-                    'ShowSpeakerLabels': True,
-                    'MaxSpeakerLabels': 2
-                }
-            )
-            logger.debug('Started Transcriptions job\n ' +
-                         json.dumps(response, default=dateconverter))
+        for record in event['Records']:
+            message = json.loads(record['Sns']['Message'])
+            for output in message['outputs']:
+                key = output['key']
+                uri = 'https://s3-' + REGION + '.amazonaws.com/' + INPUT_BUCKET + '/' + key
+                jobName = key.split('.')[0]
+                response = transcribeService.start_transcription_job(
+                    TranscriptionJobName=str(
+                        uuid.uuid4()) + '-' + getSafeGUID(jobName),
+                    LanguageCode='en-US',
+                    MediaSampleRateHertz=44100,
+                    MediaFormat='mp3',
+                    Media={
+                        'MediaFileUri': uri
+                    },
+                    OutputBucketName=OUTPUT_BUCKET,
+                    Settings={
+                        'ShowSpeakerLabels': True,
+                        'MaxSpeakerLabels': 2
+                    }
+                )
+                logger.debug('Started Transcriptions job\n ' +
+                             json.dumps(response, default=dateconverter))
+    except Exception as e:
+        logError(e, event)
+        raise
 
 
 def normalize(event, context):
     logger.debug(json.dumps(event))
+    try:
+        COMPLETE_TOPIC = os.environ['COMPLETE_TOPIC']
+        topic = sns.Topic(COMPLETE_TOPIC)
 
-    COMPLETE_TOPIC = os.environ['COMPLETE_TOPIC']
-    topic = sns.Topic(COMPLETE_TOPIC)
+        for record in event['Records']:
+            input_file_name = record['s3']['object']['key']
+            input_bucket = record['s3']['bucket']['name']
 
-    for record in event['Records']:
-        input_file_name = record['s3']['object']['key']
-        input_bucket = record['s3']['bucket']['name']
+            if input_file_name != '.write_access_check_file.temp':
+                logger.info('Starting fetching input JSON file "' +
+                            input_file_name + '" from bucket "' + input_bucket + '".')
+                input_object = s3.Object(input_bucket, input_file_name)
+                response = input_object.get()
+                logger.info('Completed fetching input JSON file "' +
+                            input_file_name + '" from bucket "' + input_bucket + '".')
 
-        if input_file_name != '.write_access_check_file.temp':
-            logger.info('Starting fetching input JSON file "' +
-                        input_file_name + '" from bucket "' + input_bucket + '".')
-            input_object = s3.Object(input_bucket, input_file_name)
-            response = input_object.get()
-            logger.info('Completed fetching input JSON file "' +
-                        input_file_name + '" from bucket "' + input_bucket + '".')
+                logger.info('Starting reading JSON from file.')
+                file_content = response['Body'].read().decode('utf-8')
+                json_content = json.loads(file_content)
+                logger.info('Completed reading JSON from file.')
 
-            logger.info('Starting reading JSON from file.')
-            file_content = response['Body'].read().decode('utf-8')
-            json_content = json.loads(file_content)
-            logger.info('Completed reading JSON from file.')
+                output_json = []
+                items = json_content["results"]["items"]
+                speaker_data = Speakers(json_content)
+                num_words = 0
+                num_punctuation = 0
 
-            output_json = []
-            items = json_content["results"]["items"]
-            speaker_data = Speakers(json_content)
-            num_words = 0
-            num_punctuation = 0
+                logger.info('Starting processing of ' +
+                            str(len(items)) + ' recognized items.')
 
-            logger.info('Starting processing of ' +
-                        str(len(items)) + ' recognized items.')
-
-            for item in items:
-                if item["type"] == "pronunciation":
-                    if "confidence" in item["alternatives"][0]:
-                        speaker = speaker_data.getNextSpeaker()
-                        output_json.append({
-                            WORD: item["alternatives"][0]["content"],
-                            CONFIDENCE: float(item["alternatives"][0]["confidence"]),
-                            START_TIME: float(item["start_time"]),
-                            END_TIME: float(item["end_time"]),
-                            SPEAKER: speaker
-                        })
-                        num_words = num_words + 1
+                for item in items:
+                    if item["type"] == "pronunciation":
+                        if "confidence" in item["alternatives"][0]:
+                            speaker = speaker_data.getNextSpeaker()
+                            output_json.append({
+                                WORD: item["alternatives"][0]["content"],
+                                CONFIDENCE: float(item["alternatives"][0]["confidence"]),
+                                START_TIME: float(item["start_time"]),
+                                END_TIME: float(item["end_time"]),
+                                SPEAKER: speaker
+                            })
+                            num_words = num_words + 1
+                        else:
+                            num_punctuation = num_punctuation + 1
                     else:
                         num_punctuation = num_punctuation + 1
-                else:
-                    num_punctuation = num_punctuation + 1
 
-            logger.info('Completed processing of ' + str(num_words) +
-                        ' words and ' + str(num_punctuation) + ' punctuation.')
+                logger.info('Completed processing of ' + str(num_words) +
+                            ' words and ' + str(num_punctuation) + ' punctuation.')
 
-            output_bucket = os.environ["OUTPUT_BUCKET"]
-            logger.info('Starting writing JSON to  "' +
-                        input_file_name + '" in bucket "' + output_bucket + '".')
-            output_file = s3.Object(output_bucket, input_file_name)
-            output_file.put(Body=json.dumps(output_json))
-            logger.info('Completed writing JSON to  "' +
-                        input_file_name + '" in bucket "' + output_bucket + '".')
+                output_bucket = os.environ["OUTPUT_BUCKET"]
+                logger.info('Starting writing JSON to  "' +
+                            input_file_name + '" in bucket "' + output_bucket + '".')
+                output_file = s3.Object(output_bucket, input_file_name)
+                output_file.put(Body=json.dumps(output_json))
+                logger.info('Completed writing JSON to  "' +
+                            input_file_name + '" in bucket "' + output_bucket + '".')
 
-            guid = input_file_name.split('-')[5]
-            logger.debug('Starting updating dynamodb record for ' + guid + '.')
-
-            episode = EpisodeModel.get(hash_key=guid)
-            if episode.splitAWSTranscriptions:
-                if input_file_name not in episode.splitAWSTranscriptions:
-                    episode.splitAWSTranscriptions.append(input_file_name)
-                    episode.save()
-            else:
-                episode.splitAWSTranscriptions = [input_file_name]
-                episode.save()
-
-            logger.debug(
-                'Completed updating dynamodb record for ' + guid + '.')
-
-            if len(episode.splitAWSTranscriptions) == len(episode.splits):
-                message = {
-                    'files': [],
-                    'guid': guid
-                }
-                for key in episode.splitAWSTranscriptions:
-                    message['files'].append(key)
-                logger.debug('Sending ' + json.dumps(message) +
-                             '\n to the AWS normalization complete SNS.')
-                response = topic.publish(Message=json.dumps(
-                    {'default': json.dumps(message)}), MessageStructure='json')
+                guid = input_file_name.split('-')[5]
                 logger.debug(
-                    'Sent message to AWS normalization complete SNS ' + json.dumps(response))
-        else:
-            logger.info('Skipping processing of file "' +
-                        input_file_name + '" from bucket "' + input_bucket + '".')
+                    'Starting updating dynamodb record for ' + guid + '.')
+
+                episode = EpisodeModel.get(hash_key=guid)
+                if episode.splitAWSTranscriptions:
+                    if input_file_name not in episode.splitAWSTranscriptions:
+                        episode.splitAWSTranscriptions.append(input_file_name)
+                        episode.save()
+                else:
+                    episode.splitAWSTranscriptions = [input_file_name]
+                    episode.save()
+
+                logger.debug(
+                    'Completed updating dynamodb record for ' + guid + '.')
+
+                if len(episode.splitAWSTranscriptions) == len(episode.splits):
+                    message = {
+                        'files': [],
+                        'guid': guid
+                    }
+                    for key in episode.splitAWSTranscriptions:
+                        message['files'].append(key)
+                    logger.debug('Sending ' + json.dumps(message) +
+                                 '\n to the AWS normalization complete SNS.')
+                    response = topic.publish(Message=json.dumps(
+                        {'default': json.dumps(message)}), MessageStructure='json')
+                    logger.debug(
+                        'Sent message to AWS normalization complete SNS ' + json.dumps(response))
+            else:
+                logger.info('Skipping processing of file "' +
+                            input_file_name + '" from bucket "' + input_bucket + '".')
+    except Exception as e:
+        logError(e, event)
+        raise
--- pipeline/functions/normalized.py
+++ pipeline/functions/normalized.py
@@ -9,6 +9,7 @@ from pynamodb.exceptions import DoesNotExist
 from functions.utils.pynamodb_models import EpisodeModel
 from functions.utils.Transcription import Transcription
 from functions.utils.log_cfg import logger
+from functions.utils.utils import logError
 
 s3 = boto3.resource('s3')
 sns = boto3.resource('sns')
@@ -16,125 +17,134 @@ sns = boto3.resource('sns')
 
 def combine(event, context):
     logger.debug(json.dumps(event))
+    try:
+        aws_bucket = os.environ["AWS_INPUT_BUCKET"]
+        watson_bucket = os.environ["WATSON_INPUT_BUCKET"]
+        output_bucket = os.environ["OUTPUT_BUCKET"]
 
-    aws_bucket = os.environ["AWS_INPUT_BUCKET"]
-    watson_bucket = os.environ["WATSON_INPUT_BUCKET"]
-    output_bucket = os.environ["OUTPUT_BUCKET"]
+        for record in event['Records']:
+            message = json.loads(record['Sns']['Message'])
+            guid = message['guid']
 
-    for record in event['Records']:
-        message = json.loads(record['Sns']['Message'])
-        guid = message['guid']
+            episode = EpisodeModel.get(hash_key=guid)
 
-        episode = EpisodeModel.get(hash_key=guid)
+            watson_filename = episode.watsonTranscription
+            aws_filename = episode.awsTranscription
 
-        watson_filename = episode.watsonTranscription
-        aws_filename = episode.awsTranscription
+            logger.info('Looking for ' + aws_filename + ' in ' + aws_bucket)
+            logger.info('Looking for ' + watson_filename +
+                        ' in ' + watson_bucket)
+            aws_object = s3.Object(aws_bucket, aws_filename)
+            watson_object = s3.Object(watson_bucket, watson_filename)
+            both_present = True
 
-        logger.info('Looking for ' + aws_filename + ' in ' + aws_bucket)
-        logger.info('Looking for ' + watson_filename + ' in ' + watson_bucket)
-        aws_object = s3.Object(aws_bucket, aws_filename)
-        watson_object = s3.Object(watson_bucket, watson_filename)
-        both_present = True
+            try:
+                aws_object.load()
+                watson_object.load()
+            except botocore.exceptions.ClientError:
+                both_present = False
 
-        try:
-            aws_object.load()
-            watson_object.load()
-        except botocore.exceptions.ClientError:
-            both_present = False
+            if both_present:
+                logger.info('Starting fetching the normalized JSON files.')
+                aws_response = aws_object.get()
+                logger.info('Completed fetching the normalized AWS JSON file.')
+                watson_response = watson_object.get()
+                logger.info(
+                    'Completed fetching the normalized Watson JSON file.')
 
-        if both_present:
-            logger.info('Starting fetching the normalized JSON files.')
-            aws_response = aws_object.get()
-            logger.info('Completed fetching the normalized AWS JSON file.')
-            watson_response = watson_object.get()
-            logger.info('Completed fetching the normalized Watson JSON file.')
+                logger.info('Starting reading JSON from files.')
+                aws_file_content = aws_response['Body'].read().decode('utf-8')
+                aws_json_content = json.loads(aws_file_content)
+                logger.info('Completed reading JSON from the AWS file.')
+                watson_file_content = watson_response['Body'].read().decode(
+                    'utf-8')
+                watson_json_content = json.loads(watson_file_content)
+                logger.info('Completed reading JSON from the Watson file.')
 
-            logger.info('Starting reading JSON from files.')
-            aws_file_content = aws_response['Body'].read().decode('utf-8')
-            aws_json_content = json.loads(aws_file_content)
-            logger.info('Completed reading JSON from the AWS file.')
-            watson_file_content = watson_response['Body'].read().decode(
-                'utf-8')
-            watson_json_content = json.loads(watson_file_content)
-            logger.info('Completed reading JSON from the Watson file.')
+                left = Transcription(watson_json_content)
+                right = Transcription(aws_json_content)
+                left.enhanceTranscription(right)
 
-            left = Transcription(watson_json_content)
-            right = Transcription(aws_json_content)
-            left.enhanceTranscription(right)
-
-            output_filename = guid + '.json'
-            logger.info('Starting writing JSON to  "' +
-                        output_filename + '" in bucket "' + output_bucket + '".')
-            output_file = s3.Object(output_bucket, output_filename)
-            output_file.put(Body=json.dumps(left.json))
-            logger.info('Completed writing JSON to  "' +
-                        output_filename + '" in bucket "' + output_bucket + '".')
-        else:
-            logger.info('Skipping combine for "' + guid +
-                        '" because at least one normalization is missing.')
+                output_filename = guid + '.json'
+                logger.info('Starting writing JSON to  "' +
+                            output_filename + '" in bucket "' + output_bucket + '".')
+                output_file = s3.Object(output_bucket, output_filename)
+                output_file.put(Body=json.dumps(left.json))
+                logger.info('Completed writing JSON to  "' +
+                            output_filename + '" in bucket "' + output_bucket + '".')
+            else:
+                logger.info('Skipping combine for "' + guid +
+                            '" because at least one normalization is missing.')
+    except Exception as e:
+        logError(e, event)
+        raise
 
 
 def zip(event, context):
     logger.debug(json.dumps(event))
+    try:
+        input_bucket = os.environ["INPUT_BUCKET"]
+        output_bucket = os.environ["OUTPUT_BUCKET"]
+        transcription_attribute = os.environ["TRANSCRIPTION_ATTRIBUTE"]
+        complete_topic = os.environ['COMPLETE_TOPIC']
+        topic = sns.Topic(complete_topic)
 
-    input_bucket = os.environ["INPUT_BUCKET"]
-    output_bucket = os.environ["OUTPUT_BUCKET"]
-    transcription_attribute = os.environ["TRANSCRIPTION_ATTRIBUTE"]
-    complete_topic = os.environ['COMPLETE_TOPIC']
-    topic = sns.Topic(complete_topic)
+        for record in event['Records']:
+            message = json.loads(record['Sns']['Message'])
 
-    for record in event['Records']:
-        message = json.loads(record['Sns']['Message'])
+            guid = message['guid']
+            outputFilename = guid + '.json'
+            transcriptions = []
 
-        guid = message['guid']
-        outputFilename = guid + '.json'
-        transcriptions = []
+            logger.info('Starting reading "' + str(len(
+                message['files'])) + '" transcription files from S3 for episode "' + guid + '".')
+            for file in message['files']:
+                fileName = file.split('.')[0]
+                startTime = int(fileName.split('-')[6])
 
-        logger.info('Starting reading "' + str(len(
-            message['files'])) + '" transcription files from S3 for episode "' + guid + '".')
-        for file in message['files']:
-            fileName = file.split('.')[0]
-            startTime = int(fileName.split('-')[6])
+                input_object = s3.Object(input_bucket, file)
+                response = input_object.get()
+                file_content = response['Body'].read().decode('utf-8')
+                json_content = json.loads(file_content)
 
-            input_object = s3.Object(input_bucket, file)
-            response = input_object.get()
-            file_content = response['Body'].read().decode('utf-8')
-            json_content = json.loads(file_content)
+                transcriptions.append(Transcription(json_content, startTime))
+            logger.info('Completed reading "' + str(len(
+                message['files'])) + '" transcription files from S3 for episode "' + guid + '".')
 
-            transcriptions.append(Transcription(json_content, startTime))
-        logger.info('Completed reading "' + str(len(
-            message['files'])) + '" transcription files from S3 for episode "' + guid + '".')
+            logger.info('Starting zipping "' + str(len(transcriptions)) +
+                        '" transcription files for episode "' + guid + '".')
+            transcriptions.sort(key=Transcription.startTimeKey)
+            first = transcriptions.pop(0)
+            for transcription in transcriptions:
+                first.appendTranscription(transcription)
+            logger.info('Completed zipping "' + str(len(transcriptions) + 1) +
+                        '" transcription files for episode "' + guid + '".')
 
-        logger.info('Starting zipping "' + str(len(transcriptions)) +
-                    '" transcription files for episode "' + guid + '".')
-        transcriptions.sort(key=Transcription.startTimeKey)
-        first = transcriptions.pop(0)
-        for transcription in transcriptions:
-            first.appendTranscription(transcription)
-        logger.info('Completed zipping "' + str(len(transcriptions) + 1) +
-                    '" transcription files for episode "' + guid + '".')
+            logger.info('Starting writing JSON to  "' +
+                        outputFilename + '" in bucket "' + output_bucket + '".')
+            outputFilename = guid + '.json'
+            output_file = s3.Object(output_bucket, outputFilename)
+            output_file.put(Body=json.dumps(first.json))
+            logger.info('Completed writing JSON to  "' +
+                        outputFilename + '" in bucket "' + output_bucket + '".')
 
-        logger.info('Starting writing JSON to  "' +
-                    outputFilename + '" in bucket "' + output_bucket + '".')
-        outputFilename = guid + '.json'
-        output_file = s3.Object(output_bucket, outputFilename)
-        output_file.put(Body=json.dumps(first.json))
-        logger.info('Completed writing JSON to  "' +
-                    outputFilename + '" in bucket "' + output_bucket + '".')
-
-        logger.debug('Starting updating dynamodb record for ' + guid + '.')
-        episode = EpisodeModel.get(hash_key=guid)
-        setattr(episode, transcription_attribute, outputFilename)
-        episode.save()
-        logger.debug('Completed updating dynamodb record for ' + guid + '.')
-
-        if episode.watsonTranscription and episode.awsTranscription:
-            message = {
-                'guid': guid
-            }
-            logger.debug('Sending ' + json.dumps(message) +
-                         '\n to the zip complete SNS.')
-            response = topic.publish(Message=json.dumps(
-                {'default': json.dumps(message)}), MessageStructure='json')
+            logger.debug('Starting updating dynamodb record for ' + guid + '.')
+            episode = EpisodeModel.get(hash_key=guid)
+            setattr(episode, transcription_attribute, outputFilename)
+            episode.save()
             logger.debug(
-                'Sent message to the zip complete SNS ' + json.dumps(response))
+                'Completed updating dynamodb record for ' + guid + '.')
+
+            if episode.watsonTranscription and episode.awsTranscription:
+                message = {
+                    'guid': guid
+                }
+                logger.debug('Sending ' + json.dumps(message) +
+                             '\n to the zip complete SNS.')
+                response = topic.publish(Message=json.dumps(
+                    {'default': json.dumps(message)}), MessageStructure='json')
+                logger.debug(
+                    'Sent message to the zip complete SNS ' + json.dumps(response))
+    except Exception as e:
+        logError(e, event)
+        raise
