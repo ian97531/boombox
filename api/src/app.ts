ec8ec63e552fd025970aed890f1a21c1f105b3a1
Ian White <ianwhitester@gmail.com>
2018-08-22 20:01:16 -0700

    Set up cors to accept requests from all 4 environments
    Also accepts direct API calls.

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,10 +3,29 @@ import { json, urlencoded } from 'body-parser'
 import cors = require('cors')
 import express = require('express')
 import statements from './endpoints/statements'
+import { Forbidden } from './errors'
 import { handleErrors, handleNotFound } from './middleware'
 
 const app: express.Application = express()
-app.use(cors())
+
+const domainName = process.env.DOMAIN_NAME
+const whitelist = [
+  'https://' + domainName,
+  'https://www-test.' + domainName,
+  'https://www-ian.' + domainName,
+  'https://www-andrew.' + domainName,
+]
+const corsOptions: cors.CorsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
+      callback(null, true)
+    } else {
+      callback(new Forbidden('Not allowed by CORS'))
+    }
+  },
+}
+
+app.use(cors(corsOptions))
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(eventContext())
