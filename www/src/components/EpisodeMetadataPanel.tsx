fe66be5777472b96a2e1597c0f3b9560e3a112e8
Andrew Wang-Hoyer <me@andrew-hoyer.com>
2018-09-02 16:22:36 -0400

    Adding some placeholder text to the EpisodeMetadataPanel

--- www/src/components/EpisodeMetadataPanel.tsx
+++ www/src/components/EpisodeMetadataPanel.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react'
 import './EpisodeMetadataPanel.css'
 
+const Heading: React.SFC = (props: { children: React.ReactNode }) => (
+  <h2 className="EpisodeMetadataPanel__heading">{props.children}</h2>
+)
+
+const Subtext: React.SFC = (props: { children: React.ReactNode }) => (
+  <p className="EpisodeMetadataPanel__subtext">{props.children}</p>
+)
+
 const EpisodeMetadataPanel: React.SFC = () => (
-  <div className="EpisodeMetadataPanel">Metadata...</div>
+  <div className="EpisodeMetadataPanel">
+    <Heading>H.I. #106: Water on Mars</Heading>
+    <Subtext>
+      Grey & Brady discuss the British 'heat wave', water on Mars, Trypophobia, Kit Kat Trademarks,
+      anti-dog discrimination, and YouTube's new news initiative and fake news.
+    </Subtext>
+
+    <Heading>Duration</Heading>
+    <Subtext>1 Hour 38 Minutes 43 Seconds</Subtext>
+
+    <Heading>Publish Date</Heading>
+    <Subtext>July 31st, 2018</Subtext>
+  </div>
 )
 
 export default EpisodeMetadataPanel
