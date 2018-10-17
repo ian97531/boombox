6ad80df909cb2244d5c976efc823db2389ee646a
Ian White <ianwhitester@gmail.com>
2018-10-16 17:10:14 -0700

    Comment out unused variable.

--- api/src/middleware/handleErrors.ts
+++ api/src/middleware/handleErrors.ts
@@ -2,20 +2,20 @@ import { IErrorResponse } from '@boombox/shared/src/types/responses'
 import { NextFunction, Request, Response } from 'express'
 import { IError } from '../types/error'
 
-const EXCLUDE_ENV = [
-  'AWS_ACCESS_KEY_ID',
-  'AWS_SECRET_ACCESS_KEY',
-  'AWS_SECURITY_TOKEN',
-  'AWS_SESSION_TOKEN',
-  'AWS_XRAY_CONTEXT_MISSING',
-  'AWS_XRAY_DAEMON_ADDRESS',
-  'LAMBDA_RUNTIME_DIR',
-  'LAMBDA_TASK_ROOT',
-  'LD_LIBRARY_PATH',
-  '_AWS_XRAY_DAEMON_ADDRESS',
-  '_AWS_XRAY_DAEMON_PORT',
-  '_X_AMZN_TRACE_ID',
-]
+// const EXCLUDE_ENV = [
+//   'AWS_ACCESS_KEY_ID',
+//   'AWS_SECRET_ACCESS_KEY',
+//   'AWS_SECURITY_TOKEN',
+//   'AWS_SESSION_TOKEN',
+//   'AWS_XRAY_CONTEXT_MISSING',
+//   'AWS_XRAY_DAEMON_ADDRESS',
+//   'LAMBDA_RUNTIME_DIR',
+//   'LAMBDA_TASK_ROOT',
+//   'LD_LIBRARY_PATH',
+//   '_AWS_XRAY_DAEMON_ADDRESS',
+//   '_AWS_XRAY_DAEMON_PORT',
+//   '_X_AMZN_TRACE_ID',
+// ]
 
 export default function(error: IError, req: Request, res: Response, next: NextFunction) {
   const status = error.status || 500
