11a180014c1c0998d4757fb2b8a77a3a34fd3086
Ian White <ian.o.white@gmail.com>
2018-11-17 15:58:19 -0800

    Improved the debugger helper.

--- pipeline/src/tools/normalize.ts
+++ pipeline/src/tools/normalize.ts
@@ -3,103 +3,16 @@ import * as fs from 'fs'
 import { ITranscript } from '@boombox/shared'
 import { normalized } from '../utils/transcribe'
 
-const timings = [
-  {
-    duration: 2783,
-    startTime: 0,
-  },
-  {
-    duration: 240,
-    startTime: 2663,
-  },
-  {
-    duration: 2782,
-    startTime: 2783,
-  },
-]
+const location = '/Users/iwhite/Desktop'
+const awsDir = `${location}/aws`
+const watsonDir = `${location}/watson`
 
-const segments = [
-  {
-    audio: {
-      duration: timings[0].duration,
-      filename: `./pipeline-ts/src/tools/json/audio-segment_${timings[0].startTime}_${
-        timings[0].duration
-      }.mp3`,
-      startTime: timings[0].startTime,
-    },
-    transcription: {
-      aws: {
-        filename: `./pipeline-ts/src/tools/json/aws-transcription-segment_${timings[0].startTime}_${
-          timings[0].duration
-        }.json`,
-      },
-      watson: {
-        filename: `./pipeline-ts/src/tools/json/watson-transcription-segment_${
-          timings[0].startTime
-        }_${timings[0].duration}.json`,
-        jobName: '371365f2-cdbe-11e8-890c-9f4e8ab94df6',
-      },
-    },
-  },
-  {
-    audio: {
-      duration: timings[1].duration,
-      filename: `./pipeline-ts/src/tools/json/audio-segment_${timings[1].startTime}_${
-        timings[1].duration
-      }.mp3`,
-      startTime: timings[1].startTime,
-    },
-    transcription: {
-      aws: {
-        filename: `./pipeline-ts/src/tools/json/aws-transcription-segment_${timings[1].startTime}_${
-          timings[1].duration
-        }.json`,
-      },
-      watson: {
-        filename: `./pipeline-ts/src/tools/json/watson-transcription-segment_${
-          timings[1].startTime
-        }_${timings[1].duration}.json`,
-      },
-    },
-  },
-  {
-    audio: {
-      duration: timings[2].duration,
-      filename: `./pipeline-ts/src/tools/json/audio-segment_${timings[2].startTime}_${
-        timings[2].duration
-      }.mp3`,
-      startTime: timings[2].startTime,
-    },
-    transcription: {
-      aws: {
-        filename: `./pipeline-ts/src/tools/json/aws-transcription-segment_${timings[2].startTime}_${
-          timings[2].duration
-        }.json`,
-      },
-      watson: {
-        filename: `./pipeline-ts/src/tools/json/watson-transcription-segment_${
-          timings[2].startTime
-        }_${timings[2].duration}.json`,
-        jobName: '417fea74-cdbe-11e8-9522-2789d9c9143b',
-      },
-    },
-  },
-]
-
-const awsSegments = []
-const watsonSegments = []
-
-for (const segment of segments) {
-  const awsContent = JSON.parse(
-    fs.readFileSync(segment.transcription.aws.filename, 'utf8')
-  ) as ITranscript
-  awsSegments.push(awsContent)
-
-  const watsonContent = JSON.parse(
-    fs.readFileSync(segment.transcription.watson.filename, 'utf8')
-  ) as ITranscript
-  watsonSegments.push(watsonContent)
-}
+const awsSegments = fs.readdirSync(awsDir).map(file => {
+  return JSON.parse(fs.readFileSync(`${awsDir}/${file}`, 'utf8')) as ITranscript
+})
+const watsonSegments = fs.readdirSync(watsonDir).map(file => {
+  return JSON.parse(fs.readFileSync(`${watsonDir}/${file}`, 'utf8')) as ITranscript
+})
 
 const awsTranscription = normalized.appendAllTranscriptions(awsSegments)
 const watsonTranscription = normalized.appendAllTranscriptions(watsonSegments)
